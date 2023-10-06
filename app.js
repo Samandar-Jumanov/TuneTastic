4cc844d816f5c1aea477c3625351a9edfe71b3f4
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-10-06 16:56:11 +0500

    fixing bugs

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ app.use(express.json())
 
 app.use(cors({
     origin:['http://localhost:3000/'],
-    methods : "*",
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods : "*"
 }))
 sequelize.sync().then(()=>{
     console.log('Models created ')
