ae9d265068bb8ad5961c35e819e1cd00dc039c94
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-10-06 15:04:35 +0500

    updating code

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin:'*',
+    origin:['http://localhost:3000/'],
     methods : "*",
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
