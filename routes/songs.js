8bc79efa1b77ed4db5a242b3c751e5475e638719
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-10-08 13:40:16 +0500

    added update song and delete song

--- routes/songs.js
+++ routes/songs.js
@@ -6,6 +6,8 @@ const {upload} = require('../utils/multer')
 
 songsRouter.post('/upload-song',  upload.single('file'),  songsController.uploadSong )
 songsRouter.get('/get-all-songs',   songsController.getAllSongs )
+songsRouter.put('/update-song/:songId/:userId',   songsController.updateSong )
+songsRouter.delete('/update-song/:songId/:userId',   songsController.deleteSong )
 
 module.exports ={
     songsRouter 
